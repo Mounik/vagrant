29dcdd1bf0da6d2ed3ce542fb9c055dda84c6180
Mounik <mounicou@gmail.com>
2023-07-16 13:13:00 +0200

    Add files via upload

--- jenkins/install_jenkins.sh
+++ jenkins/install_jenkins.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-yum -y update
-yum -y install epel-release
 
-# install ansible
-yum -y install ansible
+## install master consul
 
-# retrieve ansible code
-yum -y install git
-git clone https://github.com/diranetafen/cursus-devops.git
-cd cursus-devops/ansible
-ansible-galaxy install -r roles/requirements.yml
-ansible-playbook install_docker.yml
-sudo usermod -aG docker vagrant
-cd ../jenkins
-/usr/local/bin/docker-compose up -d
+IP=$(hostname -I | awk '{print $2}')
 
-if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
-then
-    echo "We are going to install zsh"
-    sudo yum -y install zsh git
-    echo "vagrant" | chsh -s /bin/zsh vagrant
-    su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-    su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-    sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
-    echo "plugins=(git docker docker-compose helm kubectl kubectx minikube colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
-    sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
-else
-    echo "The zsh is not installed on this server"
-fi
-echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+echo "START - install jenkins - "$IP
+
+echo "[1]: install jenkins"
+apt-get update -qq >/dev/null
+apt-get install -qq -y wget >/dev/null
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update -qq >/dev/null
+apt-get install -qq -y default-jre jenkins >/dev/null
+systemctl enable jenkins
+systemctl start jenkins
+
+echo "[2]: install docker"
+
+curl -fsSL https://get.docker.com | sh;
+sudo usermod -aG docker jenkins
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "END - install jenkins"
+
